3fa35cc5988209ecac94c895cabe24f97822763b
tahawy111 <amertahawy111@gmail.com>
2024-03-12 18:46:04 +0200

    .

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Application } from "express";
+import userRoutes from "./userRoutes";
 
 export default function (app: Application) {
-    // app.use("/api", auth);
-    // app.use("/api", user);
-    // app.use("/api", category);
-    // app.use("/api", blog);
-    // app.use("/api", comment);
-  }
+  app.use("/users", userRoutes);
+  // app.use("/api", user);
+  // app.use("/api", category);
+  // app.use("/api", blog);
+  // app.use("/api", comment);
+}
