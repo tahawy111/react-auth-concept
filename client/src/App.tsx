40b6bf75a062246a60a5c85c01428c9012be86e5
tahawy111 <amertahawy111@gmail.com>
2024-03-23 03:40:13 +0200

    .

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import axios from "axios";
+import { useEffect, useState } from "react";
+import Cookies from "universal-cookie";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const refresh = setInterval(() => {
+      axios
+        .get(`${import.meta.env.VITE_API_URL}/auth/refreshToken`, {
+          withCredentials: true,
+        })
+        .then((res) => {})
+        .catch((error) => {
+          setIsLoggedIn(false);
+          navigate("/login");
+        });
+    }, 5000);
+
+    return () => clearInterval(refresh);
+  }, [navigate]);
+
   return (
     <>
       <div>
@@ -18,12 +41,13 @@ function App() {
       <div className="card">
         <button
           onClick={() => {
-             axios.get(`${import.meta.env.VITE_API_URL}/note/getNote`, {
-              withCredentials: true,
-            }).then(data => {
-              console.log(data);
-              
-            })
+            axios
+              .get(`${import.meta.env.VITE_API_URL}/note/getNote`, {
+                withCredentials: true,
+              })
+              .then((data) => {
+                console.log(data);
+              });
           }}
         ></button>
         <p>
